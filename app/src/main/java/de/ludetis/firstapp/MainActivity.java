3bba5644c33e6c741483d93f775b50e31a503640
Your Name <you@example.com>
2021-01-05 20:16:14 +0200

    sdfgsdfgdf

--- app/src/main/java/de/ludetis/firstapp/MainActivity.java
+++ app/src/main/java/de/ludetis/firstapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doSomething() {
-        System.out.println("shbfkjsdhgfg");
+        System.out.println("asdfgjkasgfjshbfkjsdhgfg");
 
         System.out.println("shbfkjsdhgfg");
 
